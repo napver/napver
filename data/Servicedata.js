7b9daf5852a013cd223a936df92a6136317b40d2
napver <148480551+napver@users.noreply.github.com>
2023-10-19 22:52:13 +0600

    Add files via upload

---
+++ data/Servicedata.js
@@ -0,0 +1,53 @@
+const data ={
+    "service": [
+        {
+            "id": "0",
+            "title": "Website Development",
+            "shortDetail":"We craft custom websites and efficient web apps, tailored to your needs.",
+            "description": "We create custom websites tailored just for you, turning your ideas into stunning designs. We build efficient web apps to streamline your business, providing unique solutions to meet your needs. Additionally, we specialize in customized websites for educational institutions and online stores that drive sales, offering secure payment options and easy product management.",
+            "image": "assets\\img\\services\\web design.png",
+            "logo": "svg\\web.svg"
+        },
+        {
+            "id": "1",
+            "title": "Mobile App Development",
+            "shortDetail":"We make personalized mobile apps for your business.",
+            "description": "We specialize in creating custom mobile apps that bring your ideas to life. Whether you need an app for your business, a new project, or a unique concept, we're here to turn your vision into reality. Our team of experts is dedicated to delivering user-friendly, efficient, and innovative mobile solutions that cater to your specific needs. Let's transform your ideas into successful mobile applications that stand out in the digital world.",
+            "image": "assets\\img\\services\\Mobile App Development.png",
+            "logo": "svg\\mobileapp.svg"
+        },
+        {
+            "id": "2",
+            "title": "Website Maintenance",
+            "shortDetail": "We offer website maintenance to keep your website safe and up-to-date with regular care.",
+            "description": "We're here to keep your website running smoothly and up-to-date. We take care of all the technical stuff, so you can focus on your business. Our team ensures your website stays secure, loads fast, and looks great. Leave the maintenance to us and enjoy a worry-free online presence.",
+            "image": "assets\\img\\services\\Website Maintenance.png",
+            "logo": "svg\\web_Maintenance.svg"
+        },
+        {
+            "id": "3",
+            "title": "IoT Solution",
+            "shortDetail":"We offer IoT solutions for smarter connections.",
+            "description": "We specialize in making everyday things smarter and more connected. Our technology solutions help businesses and individuals harness the power of the Internet of Things to simplify tasks, save time, and enhance efficiency. Whether it's improving home automation, optimizing industrial processes, or enhancing healthcare, we're here to make your world more intelligent and convenient with IoT.",
+            "image": "assets\\img\\services\\iot.png",
+            "logo": "svg\\iot.svg"
+        },
+        {
+            "id": "4",
+            "title": "SEO",
+            "shortDetail": "We deliver SEO solutions to boost your online presence.",
+            "description": "Climb the search engine ranks with our SEO expertise. We help your business shine online by optimizing your website to reach more customers. Let us boost your web presence, so you can focus on what you do best.",
+            "image": "assets\\img\\services\\seo.png",
+            "logo": "svg\\seo.svg"
+        },
+        {
+            "id": "5",
+            "title": "Video Editing",
+            "shortDetail": "We offer professional video editing services.",
+            "description": "Where we transform your raw footage into captivating stories. Our talented team of editors and creatives work diligently to enhance your videos, adding effects, music, and flair, to make your content shine. Whether it's for personal memories or professional projects, we bring your vision to life. Trust us to add that extra sparkle to your videos!",
+            "image": "assets\\img\\services\\video editing.png",
+            "logo": "svg\\video editing.svg"
+        }
+    ]
+};
+export default data;
